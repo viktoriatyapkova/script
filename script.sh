919baa1051e902f7f59f4a37b6b50c761c532aaa
viktoriatyapkova <114080019+viktoriatyapkova@users.noreply.github.com>
2023-05-11 13:20:00 +0300

    Add files via upload

---
+++ script.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+i=$1
+o=$2
+d=$3
+
+if [[ ! -f $i || ! -f $o || ! -n $d ]]
+ then
+  echo "Аргументы не были переданы или они не являются файлами"
+  exit 1
+fi
+counter=0
+for line in $(cat text)
+do
+    for word in $line
+    do
+        if [[ $word = $d ]]
+        then
+        counter=$((counter+1))
+        break
+    fi
+    done
+done
+echo $counter >> result
+exit 0
+
